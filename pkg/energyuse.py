114b76d28bea9d87d0f35859a7e6b838b87b9301
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-02-03 15:18:15 +0100

    Add files via upload

--- pkg/energyuse.py
+++ pkg/energyuse.py
@@ -782,39 +782,68 @@ class EnergyUseAdapter(Adapter):
 #  API
 #
 
-    def api_get(self, api_path):
+    def api_get(self, api_path,intent='default'):
         """Returns data from the WebThings Gateway API."""
-        #if self.DEBUG:
-        #    print("GET PATH = " + str(api_path))
+        if self.DEBUG:
+            print("GET PATH = " + str(api_path))
+            #print("intent in api_get: " + str(intent))
         #print("GET TOKEN = " + str(self.token))
-        if self.persistent_data['token'] == None:
-            print("PLEASE ENTER YOUR AUTHORIZATION CODE IN THE SETTINGS PAGE")
-            return {"error": 500}
+        if self.token == None:
+            print("API GET: PLEASE ENTER YOUR AUTHORIZATION CODE IN THE SETTINGS PAGE")
+            self.set_status_on_thing("Authorization code missing, check settings")
+            return []
         
         try:
             r = requests.get(self.api_server + api_path, headers={
                   'Content-Type': 'application/json',
                   'Accept': 'application/json',
-                  'Authorization': 'Bearer ' + str(self.persistent_data['token']),
-                }, verify=False, timeout=3)
-            #if self.DEBUG:
-            #    print("API GET: " + str(r.status_code) + ", " + str(r.reason))
+                  'Authorization': 'Bearer ' + str(self.token),
+                }, verify=False, timeout=5)
+            if self.DEBUG:
+                print("API GET: " + str(r.status_code) + ", " + str(r.reason))
 
             if r.status_code != 200:
                 if self.DEBUG:
                     print("API returned a status code that was not 200. It was: " + str(r.status_code))
-                return {"error": r.status_code}
+                return {"error": str(r.status_code)}
                 
             else:
-                #if self.DEBUG:
-                #    print("API get succesfull: " + str(r.text))
+                to_return = r.text
+                try:
+                    if self.DEBUG:
+                        print("api_get: received: " + str(r))
+                    #for prop_name in r:
+                    #    print(" -> " + str(prop_name))
+                    if not '{' in r.text:
+                        if self.DEBUG:
+                            print("api_get: response was not json (gateway 1.1.0 does that). Turning into json...")
+                        
+                        if 'things/' in api_path and '/properties/' in api_path:
+                            if self.DEBUG:
+                                print("properties was in api path: " + str(api_path))
+                            likely_property_name = api_path.rsplit('/', 1)[-1]
+                            to_return = {}
+                            to_return[ likely_property_name ] = json.loads(r.text)
+                            if self.DEBUG:
+                                print("returning fixed: " + str(to_return))
+                            return to_return
+                                
+                except Exception as ex:
+                    print("api_get_fix error: " + str(ex))
+                        
+                if self.DEBUG:
+                    print("returning without 1.1.0 fix")
                 return json.loads(r.text)
             
         except Exception as ex:
+            print("Error doing http request/loading returned json: " + str(ex))
+            
             if self.DEBUG:
-                print("Error doing " + str(api_path) + " request/loading returned json: " + str(ex))
+                self.speak("I could not connect to API. ", intent=intent)
             #return [] # or should this be {} ? Depends on the call perhaps.
             return {"error": 500}
+
+
 
 
 
